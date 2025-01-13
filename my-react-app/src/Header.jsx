d1df6f03ba04aa97c3a9e769d17c46714ec90108
jerryzhou <122940736+kdfchse@users.noreply.github.com>
2025-01-13 13:08:59 -0800

    init homepage(temp)

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+
+    return(
+        <header>
+            <h1>Hi, I'm Jerry Zhou</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
