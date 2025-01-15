59b2c032fdc28d697512396474d15e33cdaf08e4
jerryzhou <122940736+kdfchse@users.noreply.github.com>
2025-01-14 22:28:20 -0800

    tailwind setup complete

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -5,6 +5,7 @@ function Header(){
         <header>
             <h1>Hi, I'm Jerry Zhou</h1>
             <nav>
+            <p className="text-sm font-bold underline"> Hello world!</p>
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
