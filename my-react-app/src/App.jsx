65a4cf36678b3552bd09ebd7967ce441dcdf82e6
Jerry Zhou <122940736+kdfchse@users.noreply.github.com>
2025-01-18 16:16:41 -0800

    Init multi-page dom

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,27 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import About from './pages/About.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Index from './pages/Index.jsx'
 
 import './App.css'
 
 function App() {
   
   return(
-    <>
-      <Header/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element ={<About/>} />
+      </Routes>
+      {/*<Header/>
+      <Footer/>*/}
+    </BrowserRouter>
   )
 }
 
