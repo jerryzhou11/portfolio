f7b2f7eec63b9a15e15b7ec923f0fd9b3bf8fc31
Jerry Zhou <122940736+kdfchse@users.noreply.github.com>
2025-01-30 01:33:24 -0800

    populate projects page with items

---
+++ my-react-app/src/components/ProjectWrapper.jsx
@@ -0,0 +1,26 @@
+import CRTText from './CRTText.jsx';
+import React, { useState } from 'react';
+
+function ProjectWrapper({children, name, enableEffects=true, link}){
+    const [isHovered, setIsHovered] = useState(false);
+
+    return (
+        //Outer container with fixed height
+        <div className="flex flex-col">
+            
+            <a className="flex flex-col items-center " href={link} target="_blank" onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
+                <div className="text-xl h-6 mb-4 ">
+                    <div className={`${isHovered ? 'opacity-100' : 'opacity-0'}`}>
+                        <CRTText isEnabled={enableEffects}>
+                           {name}
+                        </CRTText>
+                    </div>
+                </div>
+                {children}
+            </a>
+        </div>
+    );
+};
+
+export default ProjectWrapper
--- my-react-app/src/pages/About.jsx
+++ my-react-app/src/pages/About.jsx
@@ -1,7 +1,10 @@
 import CRTScanlines from '../components/CRTScanlines.jsx';
 import ArcadeMachine from '../components/ArcadeMachine.jsx';
+import LizardLegacy from '../components/LizardLegacy.jsx';
 import CRTText from '../components/CRTText.jsx';
+import ProjectWrapper from '../components/ProjectWrapper.jsx';
 import lizard from '../assets/lizard.gif'
+import temp from '../assets/react.svg'
 
 
 function About({enableEffects=true}){
@@ -14,17 +17,13 @@ function About({enableEffects=true}){
                        I'm a <CRTText.Span color="neon" className="text-neon" isEnabled={enableEffects}> developer. </CRTText.Span>
                     </CRTText>
                 </div>
-                <div className="mt-20 flex flex-row">
-                    <a className="flex flex-col justify-center items-center" href="https://stolenquotient2.itch.io/lizard-legacy" target="_blank">
-                        <div className="text-xl">
-                            <CRTText isEnabled={enableEffects}>
-                                Lizard's Legacy
-                            </CRTText>
-                        </div>
-                        <div className="mt-5">
-                            <img src={lizard} alt="Lizard's Legacy" width="50" height="60"/>
-                        </div>
-                    </a>
+                <div className="mt-10 flex flex-row gap-10">
+                    <ProjectWrapper name="Lizard's Legacy" enableEffects={enableEffects} link="https://stolenquotient2.itch.io/lizard-legacy">
+                        <img src={lizard} alt="Lizard's Legacy" width="50"/>
+                    </ProjectWrapper>
+                    <ProjectWrapper name="News Redesign" enableEffects={enableEffects} link="">
+                        <img src={temp} alt="Placeholder" width="70"/>
+                    </ProjectWrapper>
                 </div>
             </ArcadeMachine>
         </CRTScanlines>
