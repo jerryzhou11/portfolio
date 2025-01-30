54c9741734b4401bd978b1cead59cbed9feb823f
Jerry Zhou <122940736+kdfchse@users.noreply.github.com>
2025-01-29 16:42:36 -0800

    lizard's legacy project

--- my-react-app/src/pages/About.jsx
+++ my-react-app/src/pages/About.jsx
@@ -1,6 +1,8 @@
 import CRTScanlines from '../components/CRTScanlines.jsx';
 import ArcadeMachine from '../components/ArcadeMachine.jsx';
 import CRTText from '../components/CRTText.jsx';
+import lizard from '../assets/lizard.gif'
+
 
 function About({enableEffects=true}){
 
@@ -11,6 +13,18 @@ function About({enableEffects=true}){
                     <CRTText isEnabled={enableEffects}>
                        I'm a <CRTText.Span color="neon" className="text-neon" isEnabled={enableEffects}> developer. </CRTText.Span>
                     </CRTText>
+                </div>
+                <div className="mt-20 flex flex-row">
+                    <a className="flex flex-col justify-center items-center" href="https://stolenquotient2.itch.io/lizard-legacy" target="_blank">
+                        <div className="text-xl">
+                            <CRTText isEnabled={enableEffects}>
+                                Lizard's Legacy
+                            </CRTText>
+                        </div>
+                        <div className="mt-5">
+                            <img src={lizard} alt="Lizard's Legacy" width="50" height="60"/>
+                        </div>
+                    </a>
                 </div>
             </ArcadeMachine>
         </CRTScanlines>
