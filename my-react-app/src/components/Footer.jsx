6e3bb2067a9d6a553379deef0b8646bf937c38bf
Jerry Zhou <122940736+kdfchse@users.noreply.github.com>
2025-02-20 01:30:03 -0800

    footer update

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer(){
     return(
         <div className="relative z-20 text-gray-600">
             <footer>
-                <p>&copy; {new Date().getFullYear()} Jerry Zhou</p>
+                <p>&copy; {new Date().getFullYear()} Jerry Zhou. Built with React + Vite</p>
             </footer>
         </div>
     );
