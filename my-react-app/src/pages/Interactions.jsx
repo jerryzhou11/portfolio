cf96fdcc6ae5ca7ff6c1e0e1764e27dca70b3a27
Jerry Zhou <122940736+jerryzhou11@users.noreply.github.com>
2025-09-17 17:55:29 -0500

    Updated Interactions.jsx

--- my-react-app/src/pages/Interactions.jsx
+++ my-react-app/src/pages/Interactions.jsx
@@ -2,9 +2,9 @@ import CRTScanlines from '../components/CRTScanlines.jsx';
 import ArcadeMachine from '../components/ArcadeMachine.jsx';
 import CRTText from '../components/CRTText.jsx';
 import ProjectWrapper from '../components/ProjectWrapper.jsx';
-import lizard from '../assets/lizard.gif'
-import leaf from '../assets/leaf.png'
-import temp from '../assets/react.svg'
+import lizard from '../assets/lizard.gif';
+import leaf from '../assets/leaf.png';
+import temp from '../assets/react.svg';
 import { Link } from 'react-router-dom';
 
 function Interactions({enableEffects=true}){
@@ -37,21 +37,4 @@ function Interactions({enableEffects=true}){
     );
 }
 
-export default Interactions
-                        </CRTText.Span>
-                    </Link>
-                </CRTText.H1>
-                <div className="mt-10 flex flex-row gap-20 justify-center">
-                    <ProjectWrapper name="Lizard's Legacy" enableEffects={enableEffects} link="https://stolenquotient2.itch.io/lizard-legacy">
-                        <img src={lizard} alt="Lizard Icon" width="50"/>
-                    </ProjectWrapper>
-                    <ProjectWrapper name="Plant Pals" enableEffects={enableEffects} link="https://www.plantpals.tech/">
-                        <img src={leaf} alt="Leaf Icon" width="70"/>
-                    </ProjectWrapper>
-                </div>
-            </ArcadeMachine>
-        </CRTScanlines>
-    );
-}
-
-export default Interactions
+export default Interactions;
